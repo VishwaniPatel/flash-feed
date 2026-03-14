4658e5053bfb30770134dd65443658a3c49248e2
Vishwani Patel <vishwanipatel494@gmail.com>
2026-03-14 19:28:17 +0530

    fix: vercel file

--- src/services/NewsDataService.jsx
+++ src/services/NewsDataService.jsx
@@ -1,12 +1,7 @@
-// import axios from "axios";
-//  "https://api.allorigins.win/raw?url=https://inshorts.com/api/en/news?category=";
-// // to get all news
-// export const getAllNewsData = async (category) => {
-//   const newsData = `${baseUrl}${category}&max_limit=100&include_card_data=true`;
-//   return await axios.get(`${newsData}`);
-// };
 import axios from "axios";
-
+const baseUrl = "https://inshorts.com/api/en/news?category=";
+// to get all news
 export const getAllNewsData = async (category) => {
-  return await axios.get(`/api/news?category=${category}`);
+  const newsData = `${baseUrl}${category}&max_limit=100&include_card_data=true`;
+  return await axios.get(`${newsData}`);
 };
